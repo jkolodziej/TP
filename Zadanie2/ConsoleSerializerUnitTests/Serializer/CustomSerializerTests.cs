f5cdbe9a319765e8214981b9e046d7a20ba0450e
julka <julkaa.k@gmail.com>
2020-12-13 15:56:03 +0100

    Remove unnecessary test file

--- Zadanie2/ConsoleSerializerUnitTests/Serializer/CustomSerializerTests.cs
+++ Zadanie2/ConsoleSerializerUnitTests/Serializer/CustomSerializerTests.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ConsoleSerializer.Serializer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleSerializer.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace ConsoleSerializer.Serializer.Tests
 {
