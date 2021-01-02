9e3d29cad06f9181759828dce4bb35ba17e9b000
Xarria <224340@edu.p.lodz.pl>
2021-01-02 14:52:51 +0100

    Code cleanup

--- Zadanie3/Zadanie3Tests/ExtensionMethodsTests.cs
+++ Zadanie3/Zadanie3Tests/ExtensionMethodsTests.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Zadanie3;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Zadanie3.Tests
 {
