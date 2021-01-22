ca6089fe39177c758557d38847b29443ecb766df
Xarria <224340@edu.p.lodz.pl>
2021-01-22 14:48:23 +0100

    Add LocationModel

--- Zadanie4/Logic/DataService.cs
+++ Zadanie4/Logic/DataService.cs
@@ -1,9 +1,7 @@
 ﻿using Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Service
 {
---
+++ Zadanie4/Model/LocationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class LocationModel
+    {
+        public short LocationID { get; set; }
+        public string Name { get; set; }
+        public decimal CostRate { get; set; }
+        public decimal Availability { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public LocationModel() { }
+
+        public LocationModel(short locationID, string name, decimal costRate, decimal availability, DateTime modifiedDate)
+        {
+            LocationID = locationID;
+            Name = name;
+            CostRate = costRate;
+            Availability = availability;
+            ModifiedDate = modifiedDate;
+        }
+
+    }
+}
